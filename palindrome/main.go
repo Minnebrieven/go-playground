7d09f103c79bf9b2eac5a764a8e1eae1b9685438
minnebrieven <libr.libr1711@gmail.com>
2023-02-25 01:52:15 +0700

    adding palindrome word, power of number and data-structures(wip)

--- palindrome/main.go
+++ palindrome/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func palindromeChecker(word string) bool {
+	for i := 0; i <= (len(word) / 2); i++ {
+		j := len(word) - 1 - i
+		if word[i] != word[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var theWord string
+
+	fmt.Print("Palindrome word Checker. Input your word down here: ")
+	fmt.Scanln("%s", &theWord)
+
+	fmt.Printf("\nIs your word (%s) palindrome ? %v", theWord, palindromeChecker(theWord))
 }
